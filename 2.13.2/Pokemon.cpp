20803f937b901d44a22ba3977499b5bbcf1f0e93
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-02-13 01:18:54 +0900

    完成

---
+++ 2.13.2/Pokemon.cpp
@@ -0,0 +1,17 @@
+﻿#include "Pokemon.h"
+#include <stdio.h>
+
+Pokemon::Pokemon(const char* _name)
+{
+	name = _name;
+}
+
+Pokemon::Pokemon(const Pokemon& _obj)
+{
+	this->name = _obj.name;
+}
+
+void Pokemon::PrintName(const char* _add)
+{
+	printf("%s%s", name, _add);
+}
